92d6ec7386a48f71db1bc16da7709459ec65394c
David Belnap <david.belnap6@gmail.com>
2017-08-16 17:19:37 -0600

    Completed functioning calculator but no ability to equate upon result

--- calculator.rb
+++ calculator.rb
@@ -1,41 +1,80 @@
 
 require 'pry'
 
-@calc []
+@calc = []
+@operator = ""
 
 puts "Welcome to the ruby calculator!"
 
 def first_number
-	puts "What is the first number?"
+	puts "What is the first number of your equation?"
+	puts "Note: Any letter or symbol entered will become 0."
 	@calc << gets.to_i
+	# unless 
+	# else puts "Please enter a valid number."
+	# end	
+	operator_input
 end
 
-def operator
+def operator_input
 	puts "Select an operator: +, -, *, /"
-	choice = gets.strip
-	case choice
+	oper = gets.strip
+	case oper
 		when "+"
-		
+			@operator = oper
 		when "-"
-
+			@operator = oper
 		when "*"
-
+			@operator = oper
 		when "/"
-
+			@operator = oper
 		else puts "Please select a valid operator."
+			operator_input
 	end
+	second_number
 end
 
 def second_number
 	puts "What is the second number?"
+	@calc << gets.to_i
+	result
 end
 
 def result
 	puts "Calculating..."
-
-	puts "The result of ___ ___ ___ is ___."
+	case @operator
+		when  "+"
+			result = @calc[0] + @calc[1]
+		when "-"
+			result = @calc[0] - @calc[1]
+		when "*"
+			result = @calc[0] * @calc[1]
+		when "/"
+			result = @calc[0] / @calc[1]
+	end
+	puts "The result of #{@calc[0]} #{@operator} #{@calc[1]} is #{result}"
+	@calc = []
+	@operator = ""
+	menu
 end
 
+def menu
+	puts "What would you like to do now?"
+	puts "Enter 1 to calculate another equation."
+	puts "Enter 2 to exit."
+	choice = gets.to_i
+
+	case choice
+		when 1
+		first_number
+		when 2
+		exit
+		else
+		puts "Please enter a valid input."
+	end
+end
+
+first_number
 
 
 # arr = [ +, -, *, / ]
@@ -48,5 +87,5 @@ end
 # a=get.to_i
 
 # what if user enter: 'abc'
-#a=gets.to_i
-#what is a?
+# a=gets.to_i
+# what is a?
